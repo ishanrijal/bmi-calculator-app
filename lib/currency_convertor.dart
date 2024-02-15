329d7d7421b70307facf82a9dbedb59819aab4c6
ishanrijal <ishanrijal6@gmail.com>
2024-02-15 22:11:57 +0545

    DIvided body into two column. In the left side added the image and in the left side, widgets are added

--- lib/currency_convertor.dart
+++ lib/currency_convertor.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
 import './profile_page.dart';
+import './test_page.dart';
+
+const maleButton = Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Icon(
+      Icons.male,
+      color: Colors.white,
+    ),
+    SizedBox(width: 8.0),
+    Text(
+      "Male",
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    )
+  ],
+);
 
 class CurrencyConvertor extends StatelessWidget {
   const CurrencyConvertor({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
+        leadingWidth: 100,
+        backgroundColor: const Color.fromARGB(255, 201, 139, 139),
         title: const Text(
-          "BMI Calculator",
+          "BMI Calculators",
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
         ),
-        backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+        titleSpacing: 80.0, // Add space between the title and leading icon
+        leading: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const TestPage(),
+              ),
+            );
+          },
+          child: const Text(
+            "Test Page",
+          ),
+        ),
         centerTitle: true,
         toolbarHeight: 80,
         actions: [
@@ -33,173 +65,225 @@ class CurrencyConvertor extends StatelessWidget {
       ),
       body: Container(
         margin: const EdgeInsets.all(24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Row(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8.0),
-                          bottomLeft: Radius.circular(8.0),
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 8.0,
-                      ),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.male,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 8.0),
-                          Text(
-                            "Male",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 177, 157, 157),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
                   ),
                 ),
-                const SizedBox(
-                  width: 8.0,
+                child: Image.network(
+                    'https://img.freepik.com/free-vector/man-character-standing-icon_24877-81729.jpg?w=740&t=st=1707931122~exp=1707931722~hmac=1553a89edb59ee28733542562bd1075785b97931ddde25e289b31ea71b05be79'),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 177, 157, 157),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8.0),
+                    bottomLeft: Radius.circular(8.0),
+                  ),
                 ),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const ProfilePage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                        ),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(8.0),
-                          bottomLeft: Radius.circular(8.0),
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 12.0),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.female,
-                            color: Colors.black,
-                          ),
-                          SizedBox(
-                            width: 8.0,
-                          ),
-                          Text(
-                            "Female",
-                            style: TextStyle(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: const BoxDecoration(
                               color: Colors.black,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                bottomLeft: Radius.circular(8.0),
+                              ),
                             ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 24.0),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      Image.network(
-                          'https://img.freepik.com/free-vector/man-character-standing-icon_24877-81729.jpg?w=740&t=st=1707931122~exp=1707931722~hmac=1553a89edb59ee28733542562bd1075785b97931ddde25e289b31ea71b05be79')
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 16.0),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Age',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      const SizedBox(height: 8.0),
-                      SizedBox(
-                        width: 100,
-                        child: TextFormField(
-                          textAlign: TextAlign.center,
-                          decoration: const InputDecoration(
-                            hintText: '32',
-                            border: OutlineInputBorder(),
-                          ),
-                          keyboardType: TextInputType.number,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 16.0,
-                      ),
-                      const Text(
-                        'Height',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      const SizedBox(height: 8.0),
-                      SizedBox(
-                        width: 100,
-                        child: TextFormField(
-                          textAlign: TextAlign.center,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            hintText: '179',
-                            border: OutlineInputBorder(),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 12.0,
+                              horizontal: 8.0,
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.male,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  "Male",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
+                        Expanded(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                bottomLeft: Radius.circular(8.0),
+                              ),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 12.0,
+                              horizontal: 8.0,
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.female,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  "Female",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      'Age',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    SizedBox(
+                      width: 100,
+                      child: TextFormField(
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          hintText: '32',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Text(
+                      'Height',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    SizedBox(
+                      width: 100,
+                      child: TextFormField(
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: '179',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                // Add more Expanded widgets for additional options
-              ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 300,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Calculate',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            )
+            // const SizedBox(height: 24.0),
+            // Row(
+            //   children: [
+            //     Expanded(
+            //       child: Column(
+            //         children: [
+            //           Image.network(
+            //               'https://img.freepik.com/free-vector/man-character-standing-icon_24877-81729.jpg?w=740&t=st=1707931122~exp=1707931722~hmac=1553a89edb59ee28733542562bd1075785b97931ddde25e289b31ea71b05be79')
+            //         ],
+            //       ),
+            //     ),
+            //     const SizedBox(width: 16.0),
+            //     Expanded(
+            //       child: Column(
+            //         crossAxisAlignment: CrossAxisAlignment.start,
+            //         children: [
+            //           const Text(
+            //             'Age',
+            //             style: TextStyle(
+            //               fontWeight: FontWeight.bold,
+            //               fontSize: 16.0,
+            //             ),
+            //           ),
+            //           const SizedBox(height: 8.0),
+            //           SizedBox(
+            //             width: 100,
+            //             child: TextFormField(
+            //               textAlign: TextAlign.center,
+            //               decoration: const InputDecoration(
+            //                 hintText: '32',
+            //                 border: OutlineInputBorder(),
+            //               ),
+            //               keyboardType: TextInputType.number,
+            //             ),
+            //           ),
+            //           const SizedBox(
+            //             height: 16.0,
+            //           ),
+            //           const Text(
+            //             'Height',
+            //             style: TextStyle(
+            //               fontWeight: FontWeight.bold,
+            //               fontSize: 16.0,
+            //             ),
+            //           ),
+            //           const SizedBox(height: 8.0),
+            //           SizedBox(
+            //             width: 100,
+            //             child: TextFormField(
+            //               textAlign: TextAlign.center,
+            //               keyboardType: TextInputType.number,
+            //               decoration: const InputDecoration(
+            //                 hintText: '179',
+            //                 border: OutlineInputBorder(),
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //     // Add more Expanded widgets for additional options
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     SizedBox(
+            //       width: 300,
+            //       child: TextButton(
+            //         style: TextButton.styleFrom(
+            //           backgroundColor: Colors.blue,
+            //         ),
+            //         onPressed: () {},
+            //         child: const Text(
+            //           'Calculate',
+            //           style: TextStyle(color: Colors.white),
+            //         ),
+            //       ),
+            //     )
+            //   ],
+            // )
           ],
         ),
       ),
